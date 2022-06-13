08d4281b335bbef53ebbcb918b48072e5023895a
Segowax <kosmicznybandyta@gmail.com>
2022-06-13 19:46:11 +0200

    API project added. Initial migration added. Services registered in StartUp.

--- Kolisetka.Persistence/Configurations/ProductConfiguration.cs
+++ Kolisetka.Persistence/Configurations/ProductConfiguration.cs
@@ -11,7 +11,7 @@ namespace Kolisetka.Persistence.Configurations
             builder.Property(prop => prop.Category).IsRequired();
             builder.Property(prop => prop.Description).IsRequired().HasMaxLength(1000);
             builder.Property(prop => prop.Name).IsRequired().HasMaxLength(100);
-            builder.Property(prop => prop.Price).IsRequired();
+            builder.Property(prop => prop.Price).IsRequired().HasPrecision(7, 2);
         }
     }
 }
