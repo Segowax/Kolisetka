b3c263b5f247db7c938045535d0b07ae39254e6f
Segowax <kosmicznybandyta@gmail.com>
2022-06-19 23:02:04 +0200

    Invalid product creating test added. Precision and scale validation added (product).

--- Kolisetka.Application.UnitTests/Products/Commands/CreateProductCommandHandlerTest.cs
+++ Kolisetka.Application.UnitTests/Products/Commands/CreateProductCommandHandlerTest.cs
@@ -1,11 +1,12 @@
 ﻿using AutoMapper;
 using Kolisetka.Application.Contracts.Persistence;
 using Kolisetka.Application.DTOs.DtoProduct;
+using Kolisetka.Application.Exceptions;
 using Kolisetka.Application.Features.Products.Handlers.Commands;
 using Kolisetka.Application.Features.Products.Requests.Commands;
 using Kolisetka.Application.Profiles;
 using Kolisetka.Application.UnitTests.Mocks;
-using MediatR;
+using Kolisetka.Domain;
 using Moq;
 using Shouldly;
 using System.Threading;
@@ -18,6 +19,7 @@ namespace Kolisetka.Application.UnitTests.Products.Commands
     {
         private readonly IMapper _mapper;
         private readonly Mock<IProductRepository> _mockRepo;
+        private readonly CreateProductCommandHandler _handler;
         private readonly ProductCreateDto _productDto;
 
         public CreateProductCommandHandlerTest()
@@ -29,10 +31,11 @@ namespace Kolisetka.Application.UnitTests.Products.Commands
                 c.AddProfile<MappingProfile>();
             });
             _mapper = mapperConfig.CreateMapper();
+            _handler = new CreateProductCommandHandler(_mockRepo.Object, _mapper);
 
             _productDto = new ProductCreateDto
             {
-                Category = 0,
+                Category = Category.Food,
                 Description = "Na mały głód.",
                 Name = "Zapiekanka",
                 Price = 10.00m
@@ -42,13 +45,45 @@ namespace Kolisetka.Application.UnitTests.Products.Commands
         [Fact]
         public async Task Valid_Product_Added()
         {
-            var handler = new CreateProductCommandHandler(_mockRepo.Object, _mapper);
-
-            await handler.Handle
+            await _handler.Handle
                 (new CreateProductCommand() { CreateProductDto = _productDto }, CancellationToken.None);
 
             var products = await _mockRepo.Object.GetAllAsync();
             products.Count.ShouldBe(4);
+        }
+
+        [Fact]
+        public async Task Invalid_Product_Added()
+        {
+            // invalid price
+            _productDto.Price = 10.002m;
+            ValidationException ex = await Should.ThrowAsync<ValidationException>
+                (async () => await _handler.Handle
+                    (new CreateProductCommand() { CreateProductDto = _productDto }, CancellationToken.None));
+
+            // invalid category
+            _productDto.Price = 10.00m;
+            _productDto.Category = (Category)3;
+            ex = await Should.ThrowAsync<ValidationException>
+                (async () => await _handler.Handle
+                    (new CreateProductCommand() { CreateProductDto = _productDto }, CancellationToken.None));
+
+            // invalid name
+            _productDto.Category = Category.Food;
+            _productDto.Name = null;
+            ex = await Should.ThrowAsync<ValidationException>
+                (async () => await _handler.Handle
+                    (new CreateProductCommand() { CreateProductDto = _productDto }, CancellationToken.None));
+
+            // invalid description
+            _productDto.Name = "Zapiekanka";
+            _productDto.Description = null;
+            ex = await Should.ThrowAsync<ValidationException>
+                (async () => await _handler.Handle
+                    (new CreateProductCommand() { CreateProductDto = _productDto }, CancellationToken.None));
+
+            var products = await _mockRepo.Object.GetAllAsync();
+            products.Count.ShouldBe(3);
         }
     }
 }
