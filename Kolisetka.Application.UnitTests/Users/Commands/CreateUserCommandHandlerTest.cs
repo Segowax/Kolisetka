c7360678b6ea85f14959e74dd372dc5de9673462
Segowax <kosmicznybandyta@gmail.com>
2022-07-20 17:01:04 +0200

    More tests added during creating invalid user.

--- Kolisetka.Application.UnitTests/Users/Commands/CreateUserCommandHandlerTest.cs
+++ Kolisetka.Application.UnitTests/Users/Commands/CreateUserCommandHandlerTest.cs
@@ -26,6 +26,8 @@ namespace Kolisetka.Application.UnitTests.Users.Commands
         private readonly UserCreateDto _validUserDto;
         private readonly UserCreateDto _invalidUserDto;
 
+        private string MyString;
+
         public CreateUserCommandHandlerTest()
         {
             _mockRepo = MockAuthRepository.GetAuthRepository();
@@ -51,7 +53,7 @@ namespace Kolisetka.Application.UnitTests.Users.Commands
 
             invalidUser = new UserCreateDto()
             {
-                Email = TestProperties.Resources.Test_InvalidUser_NewEmail,
+                Email = TestProperties.Resources.Test_ValidUser_NewEmail,
                 EmailConfirmed = true,
                 FirstName = TestProperties.Resources.Test_ValidUser_NewFirstName,
                 LastName = TestProperties.Resources.Test_ValidUser_NewLastName,
@@ -88,8 +90,9 @@ namespace Kolisetka.Application.UnitTests.Users.Commands
         }
 
         [Fact]
-        public async Task Register_User_With_Failure_Test()
+        public async Task Register_User_With_Invalid_Email_Test()
         {
+            _invalidUserDto.Email = TestProperties.Resources.Test_InvalidUser_NewEmail;
             var handler = new CreateUserCommandHandler(_mockRepo.Object, _mapper);
             var result = await handler.Handle
                 (new CreateUserCommand
@@ -102,11 +105,107 @@ namespace Kolisetka.Application.UnitTests.Users.Commands
 
             var users = MockAuthRepository.GetUsers();
             users.Count.ShouldBe(2);
+            result.Errors.Count.ShouldBe(1);
 
-            // invalid email
             result.Errors[0]
                 .ShouldBe(ApplicationProperties.Resources.User_Validator_Email
                     .Replace("{PropertyName}", nameof(_invalidUserDto.Email)));
+        }
+
+        [Fact]
+        public async Task Register_User_With_Invalid_FirstName_Test()
+        {
+            _invalidUserDto.FirstName = TestProperties.Resources.Test_InvalidUser_NewFirstName;
+            var handler = new CreateUserCommandHandler(_mockRepo.Object, _mapper);
+            var result = await handler.Handle
+                (new CreateUserCommand
+                {
+                    UserCreateDto = _invalidUserDto
+                }, CancellationToken.None);
+
+            result.ShouldBeOfType<BaseCommandResponse>();
+            result.Success.ShouldBeFalse();
+
+            var users = MockAuthRepository.GetUsers();
+            users.Count.ShouldBe(2);
+            result.Errors.Count.ShouldBe(1);
+
+            MyString = string.Concat(nameof(_invalidUserDto.FirstName).Select(x => char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+            MyString = ApplicationProperties.Resources.Product_Validator_TooLong.Replace("{PropertyName}", MyString);
+            MyString = MyString.Replace("{MaxLength}", "256");
+            result.Errors[0]
+                .ShouldBe(MyString);
+        }
+
+        [Fact]
+        public async Task Register_User_With_Invalid_LastName_Test()
+        {
+            _invalidUserDto.LastName = TestProperties.Resources.Test_InvalidUser_NewLastName;
+            var handler = new CreateUserCommandHandler(_mockRepo.Object, _mapper);
+            var result = await handler.Handle
+                (new CreateUserCommand
+                {
+                    UserCreateDto = _invalidUserDto
+                }, CancellationToken.None);
+
+            result.ShouldBeOfType<BaseCommandResponse>();
+            result.Success.ShouldBeFalse();
+
+            var users = MockAuthRepository.GetUsers();
+            users.Count.ShouldBe(2);
+            result.Errors.Count.ShouldBe(1);
+
+            MyString = string.Concat(nameof(_invalidUserDto.LastName).Select(x => char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+            MyString = ApplicationProperties.Resources.Product_Validator_TooLong.Replace("{PropertyName}", MyString);
+            MyString = MyString.Replace("{MaxLength}", "256");
+            result.Errors[0]
+                .ShouldBe(MyString);
+        }
+
+        [Fact]
+        public async Task Register_User_With_Invalid_Password_Test()
+        {
+            _invalidUserDto.Password = TestProperties.Resources.Test_InvalidUser_NewPassword;
+            var handler = new CreateUserCommandHandler(_mockRepo.Object, _mapper);
+            var result = await handler.Handle
+                (new CreateUserCommand
+                {
+                    UserCreateDto = _invalidUserDto
+                }, CancellationToken.None);
+
+            result.ShouldBeOfType<BaseCommandResponse>();
+            result.Success.ShouldBeFalse();
+
+            var users = MockAuthRepository.GetUsers();
+            users.Count.ShouldBe(2);
+            result.Errors.Count.ShouldBe(1);
+            result.Errors[0]
+                .ShouldBe(ApplicationProperties.Resources.User_Validator_Password
+                    .Replace("{PropertyName}", nameof(_invalidUserDto.Password)));
+        }
+
+        [Fact]
+        public async Task Register_User_With_Invalid_UserName_Test()
+        {
+            _invalidUserDto.UserName = TestProperties.Resources.Test_InvalidUser_NewUserName;
+            var handler = new CreateUserCommandHandler(_mockRepo.Object, _mapper);
+            var result = await handler.Handle
+                (new CreateUserCommand
+                {
+                    UserCreateDto = _invalidUserDto
+                }, CancellationToken.None);
+
+            result.ShouldBeOfType<BaseCommandResponse>();
+            result.Success.ShouldBeFalse();
+
+            var users = MockAuthRepository.GetUsers();
+            users.Count.ShouldBe(2);
+            result.Errors.Count.ShouldBe(1);
+            MyString = string.Concat(nameof(_invalidUserDto.UserName).Select(x => char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+            MyString = ApplicationProperties.Resources.Product_Validator_TooLong.Replace("{PropertyName}", MyString);
+            MyString = MyString.Replace("{MaxLength}", "256");
+            result.Errors[0]
+                .ShouldBe(MyString);
         }
     }
 }
