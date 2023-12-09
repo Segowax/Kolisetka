2da83540934f4b866f75fbc98340f84525239b55
Segowax <kosmicznybandyta@gmail.com>
2023-12-09 13:24:10 +0100

    fix

--- Kolisetka.API/Controllers/ProductController.cs
+++ Kolisetka.API/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Kolisetka.Application.Exceptions;
 using Kolisetka.Application.Features.Products.Requests.Commands;
 using Kolisetka.Application.Features.Products.Requests.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
